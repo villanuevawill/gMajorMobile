a335dd9d168ccffef001e6015745df8a57c83b86
William Villanueva <wvillanu89@gmail.com>
2014-07-18 19:27:46 -0700

    Began implementing a promise system to store data locally, so data appears immediately.

--- www/js/services/commentService.js
+++ www/js/services/commentService.js
@@ -1,31 +1,46 @@
 angular.module('gmajor.commentFactory', [])
 
-.factory('CommentFactory', function($http, ChatsFactory){
+.factory('CommentFactory', function($http, ChatsFactory, GridTargetFactory){
 
 
   var addNewComment = function(message){ 
 
     timestamp = new Date();
     author = 'Tyler';
-    music = ChatsFactory.currentBoard.exportGrids();
-    console.log(music);
+    music = GridTargetFactory.soundBoard.exportGrids();
 
     dataToServer = {message: message, timestamp: timestamp, author: author, music: music};
 
-
+    console.log(dataToServer);
     dataToServer = JSON.stringify(dataToServer);
-    addSong(dataToServer)
-    ChatsFactory.newChat = false;
+    return dataToServer;
 
   }
+
+  // var addAdditionalComment = function(data) {
+
+  // }
+
+  //     return $http({
+  //     method: 'GET',
+  //     url: url
+  //   })
+  //   .then(function (resp) {
+  //     return resp.data;
+  //   });
 
   var addSong = function (data) {
     return $http({
       method: 'POST',
       url: '/api/threads',
       data: data
-    });
+    })
+    .then(function (resp) {
+      return resp.data;
+    })
   };
+
+
 
   return {
 
